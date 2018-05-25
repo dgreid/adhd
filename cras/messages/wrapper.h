70505c0423e40c8423caea43670802a9366a117f
Dylan Reid <dgreid@chromium.org>
2018-05-25 05:30:18 +0000

    wip: messages

---
+++ cras/messages/wrapper.h
@@ -0,0 +1,7 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "../src/common/cras_iodev_info.h"
+#include "../src/common/cras_types.h"
